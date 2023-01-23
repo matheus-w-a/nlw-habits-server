b78772244dfb32fac9996bc8479947a8896cb731
Matheus Andrade <matheus.andrade@gesplan.com.br>
2023-01-23 20:10:51 -0300

    projeto finalizado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
-import Fastify from "fastify";
-import cors from "@fastify/cors";
-import { appRoutes } from "./routes";
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
-const app = Fastify();
+const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
 
-app.get('/', (req, res) => {
-    
-})
-
 app.listen({
-    port: 3333,
+  port: 3333,
 }).then(() => {
-    console.log("App is running!");
+  console.log('HTTP Server running!')
 })
